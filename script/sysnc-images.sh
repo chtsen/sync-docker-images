c238b1f4ea19f1740abc17e59fc801af9662e408
chtsen <46036990+chtsen@users.noreply.github.com>
2024-06-13 11:07:26 +0800

    Update sysnc-images.sh

--- script/sysnc-images.sh
+++ script/sysnc-images.sh
@@ -3,5 +3,6 @@
 while read line
 do
   image=$(echo $line |awk -F"/" '{print $NF}')
-  skopeo copy --all docker://docker.io/$line docker://registry.cn-hangzhou.aliyuncs.com/chtsen-sysnc/$image
+  # skopeo copy --all docker://docker.io/$line docker://registry.cn-hangzhou.aliyuncs.com/chtsen-sysnc/$image
+  skopeo copy --all docker://docker.io/$line docker://swr.cn-north-4.myhuaweicloud.com/chtsen-sysnc/$image
 done < ./docker-images.txt
