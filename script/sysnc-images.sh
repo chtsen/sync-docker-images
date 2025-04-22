9d84fc88171b56df210ce8b7d52baa0b86db9817
chtsen <46036990+chtsen@users.noreply.github.com>
2025-04-22 16:29:19 +0800

    Update sysnc-images.sh

--- script/sysnc-images.sh
+++ script/sysnc-images.sh
@@ -6,5 +6,5 @@ do
   # skopeo copy --all docker://docker.io/$line docker://docker.io/chtsen/$line
   skopeo copy --all docker://docker.io/$line docker://registry.cn-hangzhou.aliyuncs.com/chtsen-sysnc/$image
   # skopeo copy --all docker://docker.io/$line docker://swr.cn-east-3.myhuaweicloud.com/chtsen-sysnc/$image
-  skopeo copy --all docker://docker.io/$line docker://hub.yozosoft.com:4431/hub-sysnc/$image
+  # skopeo copy --all docker://docker.io/$line docker://hub.yozosoft.com:4431/hub-sysnc/$image
 done < ./docker-images.txt
